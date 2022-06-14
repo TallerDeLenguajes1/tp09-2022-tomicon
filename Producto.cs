0f85f1961ee8dcd8f08ba485563451290f68863d
Coronel Francisco Exequiel <execoronel14f@gmail.com>
2022-06-14 15:56:47 -0300

    Archivo json cargado con productos

---
+++ Producto.cs
@@ -0,0 +1,26 @@
+public class Producto
+{
+    private string nombre;
+    private DateTime fechavencimiento;
+    private float precio; //entre 1000 y 5000;
+    private string tamanio;
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public DateTime Fechavencimiento { get => fechavencimiento; set => fechavencimiento = value; }
+    public float Precio { get => precio; set => precio = value; }
+    public string Tamanio { get => tamanio; set => tamanio = value; }
+
+    public Producto()
+    {
+        string[] nombres= {"papas fritas","dulce de leche","vino","aceite"};
+        string[] fechas= {"30/06/2023", "28/02/2023","15/04/2023"};
+        string[] tamanios= {"pequenio","mediano","grande"};
+
+        Random a= new Random();
+        Nombre= nombres[a.Next(0,4)];
+        Fechavencimiento= Convert.ToDateTime(fechas[a.Next(0,3)]);
+        Tamanio= tamanios[a.Next(0,3)];
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,29 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingrese el path de la carpeta:");
-string path= Console.ReadLine();
-if (Directory.Exists(path))
+﻿using System.Text.Json;
+Random aleatorio= new Random();
+int cantProductos= aleatorio.Next(1,20);
+List<Producto> listado_productos= new List<Producto>();
+for (int i = 0; i < cantProductos; i++)
 {
-    List<string> archivos = new List<string>();
-    archivos = Directory.GetFiles(path).ToList();
-    Console.WriteLine("Estos son los archivos que se encuentran dentro del path:");
-    foreach (string item in archivos)
-    {
-        Console.WriteLine(item);
-    }
-    List<string> renglones = new List<string>();
-    int i=1;
-    foreach (var item in archivos)
-    {
-        string nombre= Path.GetFileNameWithoutExtension(item);
-        string extension = Path.GetExtension(item);
-        string linea = i + "," + nombre + "," + extension;
-        i++;
-        renglones.Add(linea);
-    }
-    File.WriteAllLines(@"C:\tp08-2022-tomicon\index.csv",renglones);
-} else
-{
-    Console.WriteLine("La ruta ingresada no existe");
+    Producto nuevo = new Producto();
+    listado_productos.Add(nuevo);
 }
+string todos_productos= JsonSerializer.Serialize(listado_productos);
+File.WriteAllText("index.json", todos_productos);
+
+
 
 
